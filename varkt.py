063c4634f9bd5c801d943b5e2cbeac6a61fff889
Stepan Tereshin <stepantereshin27@gmail.com>
2024-11-27 12:16:49 +0300

    Добавлена программная реализация

---
+++ varkt.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+from scipy import constants
+
+# Данные с Земли
+# m0 = 287000 + 4725 #сухая масса
+# m = 258000 # масса топлива
+# Ft = 3268861.02 # тк сами рассчитать не смогли, взяли среднее из интернета
+# Cf = 0.5
+# ro = 1.293 # плотность воздуха
+# S = constants.pi * ((10.3/2)**2)
+# g = constants.g
+# k = m/(11*60) #скорость расхода топлива (через 11 мин отсоединилась последняя ступень)
+
+# Данные с Кербина
+m0 = 46904
+M = m0 + 41482  # масса с топливом
+Ft = 3268861.02  # тк сами рассчитать не смогли, взяли среднее из интернета
+Cf = 0.5
+ro = 1.293  # плотность воздуха
+S = constants.pi * ((6.6 / 2) ** 2)
+g = 1.00034 * constants.g
+k = (M - m0) / (3 * 60 + 5)  # скорость расхода топлива (через 3 мин отсоединилась последняя ступень)
+
+
+def A(t):
+    return (Ft / (M - k * t))
+
+
+def B(t):
+    return ((Cf * ro * S) / (2 * (M - k * t)))
+
+
+def dv_dt(t, v):
+    return (A(t) - B(t) * v ** 2 - g)
+
+
+# Начальная скорость
+v0 = 0
+
+# Время интеграции
+t = np.linspace(0, 34, 1080)
+
+# Решение уравнения
+solve = integrate.solve_ivp(dv_dt, t_span=(0, max(t)), y0=[v0], t_eval=t)
+
+# Данные для построения графика
+x = solve.t
+y = solve.y[0]
+
+# Построение графика
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, color="royalblue", linestyle="--", linewidth=2, label="Скорость ракеты v(t)")
+plt.title("Зависимость скорости от времени (Данные с Кербина)", fontsize=14, fontweight="bold")
+plt.xlabel("Время, с", fontsize=12)
+plt.ylabel("Скорость, м/с", fontsize=12)
+plt.legend(loc="best", fontsize=10)
+plt.grid(True, linestyle="--", alpha=0.7)
+plt.xticks(fontsize=10)
+plt.yticks(fontsize=10)
+plt.show()
