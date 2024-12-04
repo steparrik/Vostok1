ea6015e1090995bb952b9a11865b2aede28c52ca
Stepan Tereshin <stepantereshin27@gmail.com>
2024-12-04 14:07:55 +0300

    .

---
+++ src/main/java/org/steparrik/App.java
@@ -0,0 +1,13 @@
+package org.steparrik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
